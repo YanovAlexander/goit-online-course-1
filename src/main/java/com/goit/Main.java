d8ee6191a135682c500626f361ef1e8ceb6ccae1
Oleksandr Yanov <oleksandr.yanov@thomsonreuters.com>
2021-01-13 11:44:54 +0200

    Add polymorphism example

--- src/main/java/com/goit/Main.java
+++
@@ -1,9 +0,0 @@
-package com.goit;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Issue book = new Book("TestNameBook", 100, "testAuthor");
-        System.out.println(book.toPrint());
-    }
-}
